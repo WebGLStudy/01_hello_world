989d57503a729ae007120118998184fa60c43cea
Takashi Imagire <imagire@gmail.com>
2018-11-30 13:34:05 +0900

    クリアの色を白に

--- default.js
+++ default.js
@@ -17,7 +17,7 @@
         gl = canvas.getContext('webgl2');
 
         // 画面クリア
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearColor(1.0, 0.0, 0.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
     }, false);
 
